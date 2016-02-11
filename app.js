b8d5b5e8be47b2bbdfa52147e33472835c6c8d9b
kris bulté <kris.bulte@pandora.be>
2016-02-11 19:55:17 +0100

    Added view engine variables

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+        
+});
+
 var server = app.listen(3000, function(){
     console.log('Server running');
 });
