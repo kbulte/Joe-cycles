82175cd3fcc3554b6931720d7226644e9de89ef8
kris bulté <kris.bulte@pandora.be>
2016-02-22 20:52:33 +0100

    Inject bower dependencies via wiredep

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,29 @@ var jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function () {
     
-    //Emits files matching provided glob or an array of globs. Returns a stream of Vinyl files that can be piped to plugins.
+    //Src emits files matching provided glob or an array of globs. Returns a stream of Vinyl files that can be piped to plugins.
+    
     return gulp.src(jsFiles) //return the stream so it can be used as a subtask
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
             verbose: true
         }))
         .pipe(jscs());
+});
+
+gulp.task('inject', function () {
+    
+    //wiredep checks bower.json dependencies and injects bower dependencies where it finds comment <!-- bower:js -->
+                                                                                                 //<!-- bower:css -->
+                                                                                                 //<!-- endbower -->
+    var wiredep = require('wiredep').stream;
+    var options = {
+        bowerJson : require('./bower.json'),
+        directory : './public/lib',
+        ignorePath: '../../public'
+    };
+    
+    return gulp.src('./src/views/*.ejs')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
 });
