cd119829781eb4597b78fb3ececabd076db40fb1
kris bulté <kris.bulte@pandora.be>
2016-03-07 21:47:28 +0100

    Added icomoon fonts

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.static('fonts'));
 
 app.get('/', function(req, res){
         res.render('index');
