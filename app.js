f188fa0192309ee68fd034933a0bda2604b5d38f
kris bulté <kris.bulte@pandora.be>
2016-03-08 23:05:17 +0100

    Added azure environment variables

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-app.use(express.static('fonts'));
 
 app.get('/', function(req, res){
         res.render('index');
 });
 
-
-var server = app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function(){
     console.log('Server running on port 3000');
 });
