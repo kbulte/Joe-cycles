dfdb84f8b1aed1394dc9b1efa650b4337afbe084
kris bulté <kris.bulte@pandora.be>
2016-04-08 22:20:12 +0200

    Added opened message

--- app.js
+++ app.js
@@ -7,7 +7,29 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-        res.render('index');
+    
+        
+        var openingHours = { 
+                1: { open: 16 , closed: 20},
+                2: { open: 16 , closed: 20},
+                3: { open: 16 , closed: 20},
+                4: { open: 16 , closed: 20},
+                5: { open: 0 , closed: 0},
+                6: { open: 16 , closed: 18},
+                0: { open: 0 , closed: 0},
+            };
+            
+        var now = new Date();
+        var day = now.getDay();
+        var time = now.getHours();
+        var isOpen = false;
+        var hours = openingHours[day]
+        
+        if(hours.open < time && hours.closed > time)    isOpen = true;
+    
+        res.render('index', {
+        isOpen: isOpen
+    });
 });
 
 var port = process.env.PORT || 3000;
